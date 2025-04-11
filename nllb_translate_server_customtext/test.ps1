09acdc962c3b6ae6fdf9923da88dca791c951dad
bbfox0703 <101542129+bbfox0703@users.noreply.github.com>
2025-04-11 17:39:58 +0800

    Update test.ps1

--- nllb_translate_server_customtext/test.ps1
+++ nllb_translate_server_customtext/test.ps1
@@ -3,13 +3,14 @@ $tests = @(
   @{ text = "This is a test."; from = "en"; to = "zh-tw" },
   @{ text = "こんにちは"; from = "ja"; to = "zh-tw" },
   @{ text = "こんにちは"; from = "ja"; to = "en" },
-  @{ text = "我們要開始戰爭了"; from = "zh"; to = "en" }
+  @{ text = "開始吃飯了"; from = "zh"; to = "en" }
+  @{ text = "快來，要吃飯了"; from = "zh"; to = "ja" }  
 )
 
 foreach ($item in $tests) {
   Write-Host "`n▶ 原文: $($item.text)"
   try {
-    $response = Invoke-RestMethod -Uri http://127.0.0.1:5001/translate-lite -Method Post `
+    $response = Invoke-RestMethod -Uri http://127.0.0.1:5000/translate -Method Post `
       -Body ($item | ConvertTo-Json -Compress) `
       -ContentType 'application/json'
 
